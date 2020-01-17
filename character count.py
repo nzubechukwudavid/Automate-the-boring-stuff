0e76f1dcaea070ded37e5552911906c0f7014805
nzubechukwudavid <42248102+nzubechukwudavid@users.noreply.github.com>
2020-01-17 01:49:32 +0100

    using setdefault()

---
+++ character count.py
@@ -0,0 +1,23 @@
+message = "It was a bright cold day in April, and the clocks were striking thirten."
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] += 1
+
+
+z = list(count.items())
+
+
+
+for x in range(len(z)):
+    z[x] = list(z[x])
+    
+
+print(z)
+print("\n\n\n\n")
+
+
+for i in z:
+    print()
+    print(i[0], "-->", i[1])
