5e188a4ecdcef31256f03fbdb662400280cc0a8e
nzubechukwudavid <42248102+nzubechukwudavid@users.noreply.github.com>
2020-01-17 02:54:08 +0100

    pprint module : pprint() and pformat()

---
+++ pretty character count.py
@@ -0,0 +1,18 @@
+import pprint
+message = "It was a bright cold day in April, and the clocks were striking thirten."
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] += 1
+
+
+#pprint.pprint(count)
+
+f = pprint.pformat(count)
+print(f)
+
+
+
+#pprint.pprint(someDictionaryValue)
+#print(pprint.pformat(someDictionaryValue))
