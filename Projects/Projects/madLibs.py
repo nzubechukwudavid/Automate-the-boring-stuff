87074e45eea9036c1e5dde880dae95ebbfc618f9
nzubechukwudavid <42248102+nzubechukwudavid@users.noreply.github.com>
2023-02-16 13:24:46 +0100

    update madLibs.py

--- Projects/Projects/madLibs.py
+++ Projects/Projects/madLibs.py
@@ -5,40 +5,25 @@ Create a Mad Libs program that reads in text files and lets the user add
 their own text anywhere the word ADJECTIVE, NOUN, ADVERB, or VERB
 appears in the text file
 """
-import os
-#   Open the text file and read its contents
-#   identify any adjective, noun, adverb or verb
-#   prompt the user and replace the adjectives/ nouns / verbs / adverbs
-#
-#
 import re
+file = open(r'C:\Users\Morah\Documents\madlibs\challenges\challenge1.txt')
+sample = file.read()
+file.close()
 
-madLibSample = open(r'C:\Users\Morah\Documents\madlibs\challenges\challenge1.txt')
-madLibText = madLibSample.read()
+regex = re.comple(r'ADJECTIVE|NOUN|VERB|ADVERB', re.IGNORECASE)
 
-adjectiveRegex = re.compile(r'adjective', re.IGNORECASE)
-nounRegex = re.compile(r'noun', re.IGNORECASE)
-verbRegex = re.compile(r'verb', re.IGNORECASE)
-adverbRegex = re.compile(r'adverb', re.IGNORECASE)
+while True:
+    match = regex.search(sample)
+    if match is None:
+        break
+    elif match.group().upper() == 'ADJECTIVE':
+        print("Enter an Adjective: ")
+    elif match.group().upper() == 'NOUN':
+        print("Enter a Noun: ")
+    elif match.group().upper() == 'VERB':
+        print("Enter a Verb: ")
+    elif match.group().upper() == 'ADVERB':
+        print("Enter an Adverb: ")
 
-if adjectiveRegex.match(madLibText) is not None:
-    madLibText = adjectiveRegex.sub(input("Enter an Adjective: \n"), madLibText)
-if nounRegex.match(madLibText) is not None:
-    madLibText = nounRegex.sub(input("Enter a Noun: \n"), madLibText)
-if verbRegex.match(madLibText) is not None:
-    madLibText = verbRegex.sub(input("Enter a verb: \n"), madLibText)
-if adverbRegex.match(madLibText) is not None:
-    madLibText = adverbRegex.sub(input("Enter an Adverb: \n"), madLibText)
 
-# TODO A problem above is that i cant manage situations where the user may need to
-# enter more than one different type of word. for instance when there is
-# more than one instance of noun in the madlibs challenge/text
 
-# save result to a new file
-os.chdir(r'C:\Users\Morah\Documents\madlibs\solutions')
-solutionFile = open(r'solution1.txt', 'w')
-solutionFile.write(madLibText)
-
-# close files
-madLibSample.close()
-solutionFile.close()
