8e31b4c76dd489097b7da0dda76b12f6fe5668bc
nzubechukwudavid <42248102+nzubechukwudavid@users.noreply.github.com>
2023-02-16 16:32:14 +0100

    update madLibs.py, regexSearch.py

--- Projects/Projects/regexSearch.py
+++ Projects/Projects/regexSearch.py
@@ -4,3 +4,13 @@ line that matches a user-supplied regular expression. The results should
 be printed to the screen.
 """
 import re
+import os
+
+# Function to get all .txt files in a folder:
+def getFiles(filePath):
+    folderContent = os.listdir(filePath)
+    txtFileList = []
+    for fileName in folderContent:
+        if fileName[-4:] == '.txt':
+            txtFileList.append(fileName)
+    return txtFileList
